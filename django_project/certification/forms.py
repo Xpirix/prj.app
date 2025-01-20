5fe99fc1b062c5470cf7d8f0f044581ea097dde9
Xpirix <xpirixii@gmail.com>
2025-01-20 16:28:43 +0300

    Fix lint issues

--- django_project/certification/forms.py
+++ django_project/certification/forms.py
@@ -370,9 +370,15 @@ class CourseForm(forms.ModelForm):
     # Override the `trained_competence` field to add a character limit
     trained_competence = forms.CharField(
         max_length=120,
-        help_text='Trained competence e.g. Plugin development (max 120 characters).',
-        widget=forms.TextInput(attrs={'class': 'form-control', 'maxlength': '120'}),
+        help_text=(
+            'Trained competence e.g. Plugin development '
+            '(max 120 characters).'
+        ),
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'maxlength': '120'}
+        ),
     )
+
     class Meta:
         model = Course
         fields = (
